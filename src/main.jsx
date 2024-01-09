f22dda262d8dc3aa8f43126b34b555b759aea294
Amine Ferkani <ferkaniamine08@gmail.com>
2024-01-09 22:40:07 +0100

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import { Home, Players, Vtc } from "./pages";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Players" element={<Players />} />
-      <Route path="/Players/:pid" element={<Players />} />
-      <Route path="/vtc" element={<Vtc />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Players" element={<Players />} />
+        <Route path="/Players/:pid" element={<Players />} />
+        <Route path="/vtc" element={<Vtc />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
